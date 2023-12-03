1c26f751042fc6d5535d06672e9468c537b39c1d
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-12-02 22:49:19 -0500

    some updates

--- 2023/ts/day02.ts
+++ 2023/ts/day02.ts
@@ -10,9 +10,7 @@ readFile(`${rootDir}/2023/input/02.txt`, "utf-8", (err, data) => {
   }
 
   const isPart1 = true;
-  const partNum = isPart1 ? 1 : 2;
-
-  let results: number[] = [];
+  let result = 0;
 
   const maxColorCounts = new Map([
     ["red", 12],
@@ -24,53 +22,42 @@ readFile(`${rootDir}/2023/input/02.txt`, "utf-8", (err, data) => {
     .trim()
     .split("\n")
     .forEach((line) => {
-      const [game, sets] = line.split(":");
+      const [game, sets] = line.split(": ");
       let isPossible = true;
-      const minColorCount = new Map<string, number>([
+      const minColorCount = new Map([
         ["red", 0],
         ["green", 0],
         ["blue", 0],
       ]);
-      sets
-        .trim()
-        .split(";")
-        .forEach((set) => {
-          if (!isPossible) {
-            return;
+      sets.split("; ").forEach((set) => {
+        if (!isPossible) {
+          return;
+        }
+        set.split(", ").forEach((cube) => {
+          const [count, color] = cube.split(" ");
+          const countInt = parseInt(count);
+          if (isPart1) {
+            const maxCount = maxColorCounts.get(color);
+            if (maxCount && maxCount < countInt) {
+              isPossible = false;
+              return;
+            }
+          } else {
+            const minCount = minColorCount.get(color);
+            if (minCount !== undefined) {
+              minColorCount.set(color, Math.max(minCount, countInt));
+            }
           }
-          set
-            .trim()
-            .split(", ")
-            .forEach((cube) => {
-              const [count, color] = cube.split(" ");
-              const countInt = parseInt(count);
-              if (isPart1) {
-                const maxCount = maxColorCounts.get(color);
-                if (maxCount && maxCount < countInt) {
-                  isPossible = false;
-                  return;
-                }
-              } else {
-                const minCount = minColorCount.get(color);
-                if (minCount !== undefined) {
-                  minColorCount.set(color, Math.max(minCount, countInt));
-                }
-              }
-            });
         });
+      });
 
       if (isPart1 && isPossible) {
-        results.push(parseInt(game.replace(/\D/g, "")));
-      }
-
-      if (!isPart1) {
+        result += parseInt(game.replace(/\D/g, ""));
+      } else if (!isPart1) {
         const [c1, c2, c3] = Array.from(minColorCount.values());
-        results.push(c1 * c2 * c3);
+        result += c1 * c2 * c3;
       }
     });
 
-  console.log(
-    `part ${partNum} result:`,
-    results.reduce((acc, currVal) => acc + currVal, 0),
-  );
+  console.log(`part ${isPart1 ? 1 : 2} result:`, result);
 });

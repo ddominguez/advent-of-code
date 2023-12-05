b69834d039da10596d44739cc08daae58531fe8f
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-12-05 01:21:15 -0500

    ts - year:2023, day:3, pt 2
    fml.

--- 2023/ts/day03.ts
+++ 2023/ts/day03.ts
@@ -1,75 +1,88 @@
-import { readFile } from "node:fs";
+import { readFileSync } from "node:fs";
 import { rootPath } from "@utils";
 
-type NumberPosition = {
-  val: string;
-  pos: number[];
+const data = readFileSync(`${rootPath}/2023/input/03.txt`, "utf-8");
+const grid: string[][] = [];
+const partNumbers: number[] = [];
+const isSpecialChar = (char: string) => {
+  if (!char) return false;
+  return !/^[0-9.]+$/.test(char);
 };
 
-readFile(`${rootPath}/2023/input/03.txt`, "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
+// return speacial char pos if tracked num is adjacent
+const getAdacent = (row: number, pos: number[]): number[] | null => {
+  const checkPos = [pos[0] - 1, ...pos, pos[pos.length - 1] + 1];
+  let col: number | undefined;
+  // curr row
+  col = checkPos.find((i) => isSpecialChar(grid[row][i]));
+  if (col) {
+    return [row, col];
   }
+  // prev row
+  col = checkPos.find((i) => data[row - 1] && isSpecialChar(grid[row - 1][i]));
+  if (col) {
+    return [row - 1, col];
+  }
+  // next row
+  col = checkPos.find((i) => data[row + 1] && isSpecialChar(grid[row + 1][i]));
+  if (col) {
+    return [row + 1, col];
+  }
+  return null;
+};
 
-  const isPart1 = true;
-  let result = 0;
+data
+  .trim()
+  .split("\n")
+  .forEach((line) => grid.push(Array.from(line)));
 
-  const numPositions: NumberPosition[][] = [];
-  const symbolPos: number[][] = [];
+const spMap: Record<string, number[]> = {};
+grid.forEach((row, i) => {
+  let trackedNum = "";
+  let trackedNumPos: number[] = [];
+  row.forEach((char, j) => {
+    let checkAdj = false;
+    if (parseFloat(char) >= 0) {
+      trackedNum += char;
+      trackedNumPos.push(j);
+      checkAdj = j === row.length - 1 && trackedNum.length > 0;
+    } else {
+      checkAdj = trackedNum.length > 0;
+    }
 
-  data
-    .trim()
-    .split("\n")
-    .forEach((line, row) => {
-      numPositions[row] = [];
-      symbolPos[row] = [];
-      let trackedNum = "";
-      let trackNumPos: number[] = [];
-      for (let i = 0; i < line.length; i++) {
-        let trackIt = false;
-        if (parseInt(line[i]) >= 0) {
-          trackedNum += line[i];
-          trackNumPos.push(i);
-          if (i == line.length - 1 && trackedNum) {
-            trackIt = true;
-          }
-        } else {
-          if (/^[0-9.]+$/.test(line[i]) === false) {
-            symbolPos[row].push(i);
-          }
-          if (trackedNum) {
-            trackIt = true;
+    if (checkAdj) {
+      const adjChar = getAdacent(i, trackedNumPos);
+      if (adjChar) {
+        partNumbers.push(Number(trackedNum));
+        const [sr, sc] = adjChar;
+        if (grid[sr][sc] === "*") {
+          const key = `${sr},${sc}`;
+          if (!Object.hasOwn(spMap, key)) {
+            spMap[key] = [Number(trackedNum)];
+          } else {
+            spMap[key].push(Number(trackedNum));
           }
         }
-
-        if (trackIt) {
-          numPositions[row].push({
-            val: trackedNum,
-            pos: trackNumPos,
-          });
-          trackedNum = "";
-          trackNumPos = [];
-        }
       }
-    });
-
-  numPositions.map((np, row) => {
-    np.forEach((n) => {
-      let isAdjacent = false;
-      const updatedPos = [n.pos[0] - 1, ...n.pos, n.pos[n.pos.length - 1] + 1];
-
-      if (symbolPos[row - 1]?.some((p) => updatedPos.includes(p))) {
-        isAdjacent = true;
-      } else if (symbolPos[row + 1]?.some((p) => updatedPos.includes(p))) {
-        isAdjacent = true;
-      } else if (symbolPos[row].some((p) => updatedPos.includes(p))) {
-        isAdjacent = true;
-      }
-      if (isAdjacent) {
-        result += Number(n.val);
-      }
-    });
+      trackedNum = "";
+      trackedNumPos = [];
+      checkAdj = false;
+    }
   });
-  console.log(`part ${isPart1 ? 1 : 2} result:`, result);
 });
+
+console.log(
+  "part 1:",
+  partNumbers.reduce((acc, curr) => acc + curr, 0),
+);
+
+const gearRatios: number[] = [];
+for (const [k, v] of Object.entries(spMap)) {
+  if (v.length == 2) {
+    gearRatios.push(v.reduce((acc, curr) => acc * curr, 1));
+  }
+}
+console.log(
+  "part 2:",
+  gearRatios.reduce((acc, curr) => acc + curr, 0),
+);

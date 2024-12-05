9159af95e8ec48fa68d840dfd79163c74218dec1
David Dominguez <352985+ddominguez@users.noreply.github.com>
2024-12-04 21:41:27 -0500

    updates

--- 2024/go/day04/main.go
+++ 2024/go/day04/main.go
@@ -31,24 +31,26 @@ func part1(input string) int {
 	var res int
 	for row := range height {
 		for col := range width {
-			if grid[row][col] == "X" {
-				res += findXMAS(grid, []int{row, col})
+			if grid[row][col] != "X" {
+				continue
 			}
+			res += findXMAS(grid, []int{row, col}, width-1, height-1)
 		}
 	}
 
 	return res
 }
 
-func findXMAS(grid [][]string, currPos []int) int {
+func findXMAS(grid [][]string, currPos []int, maxCol, maxRow int) int {
 	var count int
-	maxCol := len(grid[0]) - 1
-	maxRow := len(grid) - 1
 	row, col := currPos[0], currPos[1]
 	wl := len("XMAS")
+	okRight := col+(wl-1) <= maxCol
+	okLeft := col-(wl-1) >= 0
+	okUp := row-(wl-1) >= 0
+	okDown := row+(wl-1) <= maxRow
 
-	// check right
-	if col+(wl-1) <= maxCol {
+	if okRight {
 		if grid[row][col+1] == "M" &&
 			grid[row][col+2] == "A" &&
 			grid[row][col+3] == "S" {
@@ -56,8 +58,7 @@ func findXMAS(grid [][]string, currPos []int) int {
 		}
 	}
 
-	// check left
-	if col-(wl-1) >= 0 {
+	if okLeft {
 		if grid[row][col-1] == "M" &&
 			grid[row][col-2] == "A" &&
 			grid[row][col-3] == "S" {
@@ -65,56 +66,42 @@ func findXMAS(grid [][]string, currPos []int) int {
 		}
 	}
 
-	// check up
-	if row-(wl-1) >= 0 {
+	if okUp {
 		if grid[row-1][col] == "M" &&
 			grid[row-2][col] == "A" &&
 			grid[row-3][col] == "S" {
 			count++
 		}
-	}
-
-	// check down
-	if row+(wl-1) <= maxRow {
-		if grid[row+1][col] == "M" &&
-			grid[row+2][col] == "A" &&
-			grid[row+3][col] == "S" {
-			count++
-		}
-	}
-
-	// check up right
-	if row-(wl-1) >= 0 && col+(wl-1) <= maxCol {
-		if grid[row-1][col+1] == "M" &&
+		if okRight &&
+			grid[row-1][col+1] == "M" &&
 			grid[row-2][col+2] == "A" &&
 			grid[row-3][col+3] == "S" {
 			count++
 		}
-	}
-
-	// check up left
-	if row-(wl-1) >= 0 && col-(wl-1) >= 0 {
-		if grid[row-1][col-1] == "M" &&
+		if okLeft &&
+			grid[row-1][col-1] == "M" &&
 			grid[row-2][col-2] == "A" &&
 			grid[row-3][col-3] == "S" {
 			count++
 		}
 	}
 
-	// check down right
-	if row+(wl-1) <= maxRow && col+(wl-1) <= maxCol {
-		if grid[row+1][col+1] == "M" &&
+	if okDown {
+		if grid[row+1][col] == "M" &&
+			grid[row+2][col] == "A" &&
+			grid[row+3][col] == "S" {
+			count++
+		}
+		if okRight &&
+			grid[row+1][col+1] == "M" &&
 			grid[row+2][col+2] == "A" &&
 			grid[row+3][col+3] == "S" {
 			count++
 		}
-	}
-
-	// check down left
-	if row+(wl-1) <= maxRow && col-(wl-1) >= 0 {
-		if grid[row+1][col-1] == "M" &&
-            grid[row+2][col-2] == "A" &&
-            grid[row+3][col-3] == "S" {
+		if okLeft &&
+			grid[row+1][col-1] == "M" &&
+			grid[row+2][col-2] == "A" &&
+			grid[row+3][col-3] == "S" {
 			count++
 		}
 	}

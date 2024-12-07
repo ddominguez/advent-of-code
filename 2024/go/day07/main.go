9d1ada23cb35a5fc4d0fd9337387044775bd7195
David Dominguez <352985+ddominguez@users.noreply.github.com>
2024-12-07 18:00:13 -0500

    go - year:2024, day:7, pt1

---
+++ 2024/go/day07/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var combos = make(map[int][][]string)
+
+func main() {
+	part := flag.Int("part", 1, "")
+	flag.Parse()
+	data, _ := os.ReadFile("../../input/07.txt")
+
+	if *part == 2 {
+		fmt.Println(part2(string(data)))
+	} else {
+		fmt.Println(part1(string(data)))
+	}
+}
+
+func part1(input string) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	var validVals []int
+	for _, line := range lines {
+		testValue, nums := parse(line)
+		var opCombos [][]string
+		if combos[len(nums)-1] != nil {
+			opCombos = combos[len(nums)-1]
+		} else {
+			opCombos = generateCombos(len(nums) - 1)
+			combos[len(nums)-1] = opCombos
+		}
+
+		if hasValidEquation(nums, opCombos, testValue) {
+			validVals = append(validVals, testValue)
+		}
+	}
+
+	var result int
+	for _, v := range validVals {
+		result += v
+	}
+	return result
+}
+
+func part2(input string) int {
+	var result int
+	return result
+}
+
+func hasValidEquation(nums []int, opCombos [][]string, testVal int) bool {
+	for _, ops := range opCombos {
+        calcTotal := 0
+		for i, num := range nums {
+			if i == 0 {
+				calcTotal = num
+				continue
+			}
+			calcTotal = doOp(calcTotal, num, ops[i-1])
+		}
+		if calcTotal == testVal {
+			return true
+		}
+	}
+	return false
+}
+
+func doOp(a, b int, op string) int {
+	if op == "*" {
+		return a * b
+	}
+	return a + b
+}
+
+func parse(line string) (int, []int) {
+	split := strings.Split(line, ":")
+	val, _ := strconv.Atoi(split[0])
+	nums := []int{}
+	for _, v := range strings.Fields(split[1]) {
+		n, _ := strconv.Atoi(v)
+		nums = append(nums, n)
+	}
+	return val, nums
+}
+
+func generateCombos(length int) [][]string {
+	if length <= 0 {
+		return nil
+	}
+
+	var generate func(current []string, depth int)
+	combinations := [][]string{}
+
+	generate = func(current []string, depth int) {
+		if depth == length {
+			combination := make([]string, len(current))
+			copy(combination, current)
+			combinations = append(combinations, combination)
+			return
+		}
+		generate(append(current, "+"), depth+1)
+		generate(append(current, "*"), depth+1)
+	}
+
+	generate([]string{}, 0)
+	return combinations
+}
---
+++ 2024/go/day07/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+const input = `
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
+
+func TestPart1(t *testing.T) {
+	result := part1(input)
+	expected := 3749
+	if result != expected {
+		t.Fatalf("expected %d, got %d", expected, result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := part2(input)
+	expected := -100
+	if result != expected {
+		t.Fatalf("expected %d, got %d", expected, result)
+	}
+}

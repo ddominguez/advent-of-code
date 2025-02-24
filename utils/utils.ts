9a84229de77d15b17a47b845f02a8226664c2e36
David Dominguez <352985+ddominguez@users.noreply.github.com>
2025-02-24 15:05:15 -0500

    removed utils

--- utils/utils.ts
+++
@@ -1,6 +0,0 @@
-function getRootPath() {
-  const proc = Bun.spawnSync(["git", "rev-parse", "--show-toplevel"]);
-  return proc.stdout.toString().trim();
-}
-
-export const rootPath = getRootPath();

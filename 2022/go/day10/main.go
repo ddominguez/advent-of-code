93e0c53af0f5ebd3023f585cde74f04a5ecfca6e
David Dominguez <352985+ddominguez@users.noreply.github.com>
2026-02-09 22:51:03 -0500

    2022/go: added tests for day 10

--- 2022/go/day10/main.go
+++ 2022/go/day10/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -8,58 +9,88 @@ import (
 )
 
 func main() {
+	part := flag.Int("part", 1, "")
+	flag.Parse()
 	data, _ := os.ReadFile("../../input/10.txt")
 
-	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-	ops := []int{1, 2}
+	if *part == 2 {
+		fmt.Println(part2(string(data)))
+	} else {
+		fmt.Println(part1(string(data)))
+	}
+}
+
+type Instruction struct {
+	addx   int
+	cycles int
+}
+
+func newInstruction(s string) Instruction {
+	if s == "noop" {
+		return Instruction{cycles: 1}
+	}
+	split := strings.Split(s, " ")
+	v, _ := strconv.Atoi(split[1])
+	return Instruction{addx: v, cycles: 2}
+}
+
+func part1(input string) int {
+	var signalStrength int
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	initialCycleCheck := 20
+	cycleInterval := 40
+	cycleCheck := initialCycleCheck
 	x := 1
-	cc := 0
-	ss := 0
-	ccheck := 20
-	climit := 220
-
-	// for pt2
-	crtRow := ""
-	pixelPos := 0
-
+	cycle := 0
 	for i := range lines {
-		isNoop := lines[i] == "noop"
-		split := strings.Split(lines[i], " ")
-		v := 0
-		if !isNoop {
-			v, _ = strconv.Atoi(split[1])
+		instruction := newInstruction(lines[i])
+
+		for c := range instruction.cycles {
+			cycle += 1
+			if cycle == cycleCheck {
+				signalStrength += cycle * x
+				cycleCheck += cycleInterval
+			}
+
+			if c+1 == instruction.cycles {
+				x += instruction.addx
+			}
 		}
-		for j := range ops {
-			cc += 1
-			// pt2
-			pixelChar := "."
+	}
+	return signalStrength
+}
+
+func part2(input string) string {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	var crtRowSB strings.Builder
+	crtRows := []string{}
+	crtWidth := 40
+	pixelLit := "#"
+	pixelDark := "."
+	pixelPos := 0
+	x := 1
+	for i := range lines {
+		instruction := newInstruction(lines[i])
+
+		for c := range instruction.cycles {
 			if pixelPos == x-1 || pixelPos == x || pixelPos == x+1 {
-				pixelChar = "#"
+				crtRowSB.WriteString(pixelLit)
+			} else {
+				crtRowSB.WriteString(pixelDark)
 			}
-			crtRow += pixelChar
-			// end pt2
-			if cc == ccheck {
-				ss += cc * x
-				if cc < climit {
-					ccheck += 40
-				}
-			}
-			if j > 0 {
-				x += v
-			}
-			// pt2
+
 			pixelPos += 1
-			if pixelPos%40 == 0 {
-				fmt.Println(crtRow)
-				crtRow = ""
+			if pixelPos == crtWidth {
+				crtRows = append(crtRows, crtRowSB.String())
+				crtRowSB.Reset()
 				pixelPos = 0
 			}
-			// end pt2
-			if isNoop {
-				break
+
+			if c+1 == instruction.cycles {
+				x += instruction.addx
 			}
 		}
 	}
 
-	fmt.Println("signal strengths:", ss)
+	return strings.Join(crtRows, "\n")
 }
